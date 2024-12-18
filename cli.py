337a6113c668ef4f8321f80123079d4facda81af
Racheal <rachealnjugush@gmail.com>
2024-12-18 09:31:09 +0300

    successfully autogenerate tables

--- cli.py
+++ cli.py
@@ -1,11 +1,13 @@
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models import Base,Doctor,Patient
+from models import Base,Doctor,Patient,Appointment
 
-DATABASE_URL = "sqlite:///patient.db"
+DATABASE_URL = "sqlite:///patients.db"
 
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
 session = Session()
+
+
 
