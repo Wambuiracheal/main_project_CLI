85ef92bffa0d73298ec851d0dbf4935035711a9e
Racheal <rachealnjugush@gmail.com>
2024-12-18 09:56:59 +0300

    create delete operation

--- cli.py
+++ cli.py
@@ -13,6 +13,8 @@ def init_db():
     Base.metadata.create_all(engine)
     print("Successful initialization of the database")
 
+# DOCTOR CRUD OPERATION
+
 def create_doctor():
     name = input("Enter Patient name: ")
     email = input("Enter Patient Email: ")
@@ -30,5 +32,21 @@ def update_doctor():
     doctor.name = input(f"Update doctor's name: (current: {doctor.name}): ") or doctor.name
     doctor.email = input(f"Update doctor's email: (current: {doctor.email}): ") or doctor.email
     session.commit()
-    pritn(f"Doctor ID: {doctor_id} has been updated successfully!!!")
+    print(f"Doctor ID: {doctor_id} has been updated successfully!!!")
+
+def delete_doctor():
+    doctor_id = int(input("Enter the doctor's ID to delete: "))
+    doctor = session.get(Doctor,doctor_id)
+
+    if not doctor:
+        print(f"Doctor with ID {doctor_id} does not exist.")
+        return
+    session.delete(doctor)
+    session.commit()
+    print(f"Doctor ID {doctor_id} deleted successfully.")
+
+
+if __name__== "__main__":
+    init_db()
+    
 
